56a01c3dc7c65aeaa79fca362bcda51e0db13f7c
m00se <housam8@gmail.com>
2023-10-27 15:22:00 +1100

    updated backup.sh to include sleep commands
    updated backup.sh to have sleep commands. When the changes are of a larger size, there is not enough time for the git add . command to stage the files for commit resulting in an error.

--- backup.sh
+++ backup.sh
@@ -62,12 +62,17 @@ grab_version(){
 push_config(){
   cd $config_folder
   echo Pushing updates
+  sleep 1
   git pull -v
+  sleep 1
   git add . -v
+  sleep 1
   current_date=$(date +"%Y-%m-%d %T")
   git commit -m "Backup triggered on $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
   git push
 }
 
+sleep 1
 grab_version
+sleep 1
 push_config
